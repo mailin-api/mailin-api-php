648218e32064ae5d0b4aec514d164a1ee0a3b6df
Dipankar Sarkar <dipankarsarkar@gmail.com>
2013-09-11 13:16:14 +0530

    Added the tutorial files

---
+++ examples/README.md
@@ -0,0 +1,5 @@
+# Examples
+
+ * tutorial.php - Sending a transactional email
+ * tutorial1.php - Creating campaigns and other operations
+ * tutorial2.php - Creating Lists and other operations
--- examples/tutorial.php
+++ examples/tutorial.php
@@ -1,8 +1,15 @@
 <?php
 
-include("../mailin.php");
-
-$mailin = new Mailin("http://api.mailinblue.com","youraccesskey","yoursecretkey");
-$campaigns = $mailin->get_campaigns();
+require('../mailin.php');
+/*
+ * This will initiate the API with the endpoint and your access and secret key.
+ *
+ */
+$mailin = new Mailin('http://api.mailinblue.com/v1.0','Your access key','Your secret key');
+/*
+ * This will send an email to to@email.com, without any CC or BCC without any attachements.
+ *
+ */
+$mailin->send_email({"to@email.com":"name1"},{},{},[],{"replyto@email.com":"Reply name"},"Subject","Text body","HTML body",[]);
 
 ?>
