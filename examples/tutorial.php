a009a678b6fe66f222d5a418581692fed29cd898
Dipankar Sarkar <me@dipankar.name>
2013-10-23 02:14:32 +0530

    Update tutorial.php

--- examples/tutorial.php
+++ examples/tutorial.php
@@ -10,6 +10,6 @@ $mailin = new Mailin('http://api.mailinblue.com/v1.0','Your access key','Your se
  * This will send an email to to@email.com, without any CC or BCC without any attachements.
  *
  */
-$mailin->send_email({"to@email.com":"name1"},{},{},[],{"replyto@email.com":"Reply name"},"Subject","Text body","HTML body",[]);
+$mailin->send_email(array("test1@gmail.com"=>"name1"),array(),array(),array(),array("test2@gmail.com"=>"Reply name"),"Subject","Text body","HTML body",array());
 
 ?>
