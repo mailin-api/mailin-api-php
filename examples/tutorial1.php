9abe4300156e9049e85bff43f53cf05cc2a820c5
Dipankar Sarkar <me@dipankar.name>
2013-10-28 12:16:04 +0530

    Update tutorial1.php
    Removing names

--- examples/tutorial1.php
+++ examples/tutorial1.php
@@ -10,6 +10,6 @@ $mailin = new Mailin('https://api.mailinblue.com/v1.0','Your access key','Your s
  * This will send an email to to@email.com, without any CC or BCC without any attachements.
  *
  */
-var_dump($mailin->send_email(array(),"This is the text",array(),array("test1@gmail.com","reply to!"),"This is the HTML",array("to@email.com"=>"Dipankar"),array(),array("super@desinerd.com","sender email!"),"Subject"));
+var_dump($mailin->send_email(array(),"This is the text",array(),array("replyto@email.com","reply to!"),"This is the HTML",array("to@email.com"=>"to whom!"),array(),array("sender@email.com","sender email!"),"Subject"));
 
 ?>
