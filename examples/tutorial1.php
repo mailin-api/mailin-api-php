7506754cc028296993833ef3bfacd7d25ae6ea5c
Dipankar Sarkar <dipankarsarkar@gmail.com>
2013-11-13 22:31:45 +0530

    Fixed extra bracket

--- examples/tutorial1.php
+++ examples/tutorial1.php
@@ -10,5 +10,5 @@ $mailin = new Mailin('https://api.mailinblue.com/v1.0','Your access key','Your s
  * This will send an email to to@email.com, without any CC or BCC without any attachements.
  *
  */
-var_dump($mailin->send_email(array("to@email.com"=>"to whom!"),"Subject",array("sender@email.com","sender email!"),"This is the HTML","This is the text",array(),array(),array("replyto@email.com","reply to!"),array());
+var_dump($mailin->send_email(array("to@email.com"=>"to whom!"),"Subject",array("sender@email.com","sender email!"),"This is the HTML","This is the text",array(),array(),array("replyto@email.com","reply to!"),array()));
 ?>
