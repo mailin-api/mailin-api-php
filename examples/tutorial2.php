54a7de82a5a3243d9075869d80cebd2a1e92aca9
elmahdi <elmahdi.khokha@dualtech.fr>
2014-03-06 12:17:43 +0100

    Update the wording from older name to sendinblue

--- examples/tutorial2.php
+++ examples/tutorial2.php
@@ -5,7 +5,7 @@ require('../mailin.php');
  * This will initiate the API with the endpoint and your access and secret key.
  *
  */
-$mailin = new Mailin('https://api.mailinblue.com/v1.0','Your access key','Your secret key');
+$mailin = new Mailin('https://api.sendinblue.com/v1.0','Your access key','Your secret key');
 /*
  * This will get all your campaigns
  *
